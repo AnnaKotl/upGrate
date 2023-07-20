bef846f165058194bd4c88835980eb6ba32cd9d6
AnnaKotl <123831146+AnnaKotl@users.noreply.github.com>
2023-07-20 21:14:52 +0000

    Deploying to gh-pages from @ AnnaKotl/upGrate@667c1418568af2cd12065f04d517f3b56b21b25d 🚀

--- favorites.c988b6d9.js
+++
@@ -1,2 +0,0 @@
-console.log("favourite-foods.js work"),console.log("header.js work"),console.log("hero-switch.js work"),console.log("her.js work"),console.log("modal-order-now.js work"),console.log("popular-recipes.js work"),console.log("recipe-placeholder.js work"),console.log("scroll-categories.js work"),console.log("1 JS work"),console.log("2 JS work"),console.log("3 JS work"),console.log("4 JS work"),console.log("5 JS work");
-//# sourceMappingURL=favorites.c988b6d9.js.map
